0c6a9643ce53dc007594d39920dfe868786e151f
Uladzislau Khamkou <kham@tuta.io>
2022-03-18 14:11:14 +0300

    implement Register class

---
+++ interpreter.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Register:
+    value: int
+
+    @property
+    def low(self):
+        return self.value % 256
+
+    @property
+    def hieght(self):
+        return (self.value // 256) % 256
+
+    @property
+    def value32(self):
+        return self.value % 2**32
+
+    @low.setter
+    def low(self, value):
+        self.value += (value % 256) - self.low
+
+    @hieght.setter
+    def hieght(self, value):
+        self.value += (value % 256 - self.hieght) * 256
+
+    @value32.setter
+    def value32(self, value):
+        self.value += value % 2**32 - self.value32
+
+    def __str__(self):
+        return f"R({self.value})"
