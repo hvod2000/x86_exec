15b3a34f899c94f32becc30777f8edbe513655ac
Uladzislau Khamkou <kham@tuta.io>
2022-03-26 01:21:50 +0300

    implement ChainDict

---
+++ mycollections.py
@@ -0,0 +1,27 @@
+from collections import ChainMap
+
+
+class ChainDict:
+    def __init__(self, *dicts):
+        self.dicts = dicts
+
+    def __getitem__(self, i):
+        for d in self.dicts:
+            if i in d:
+                return d[i]
+        raise KeyError(i)
+
+    def __setitem__(self, i, value):
+        try:
+            next(d for d in self.dicts if i in d)[i] = value
+        except StopIteration:
+            raise KeyError(i)
+
+    def items(self):
+        visited = set()
+        for d in self.dicts:
+            for k, v in d.items():
+                if k in visited:
+                    continue
+                visited.add(k)
+                yield k, v
