4b7d8a6e76c680b9764800aac39a653f0e7a3063
Uladzislau Khamkou <kham@tuta.io>
2022-03-25 22:42:48 +0300

    move the in-memory integers into separate module

--- interpreter.py
+++ interpreter.py
@@ -1,78 +1,9 @@
 import dataclasses
 from mymath import ceil_log
+from memory import IntValue, Int
 from string import ascii_lowercase, digits
 from instructions import Instruction
 from collections import ChainMap
-
-
-@dataclasses.dataclass
-class IntValue:
-    value: int
-    size: int
-
-    def __init__(self, value, size):
-        self.value = value % 2 ** (size * 8)
-        self.size = size
-
-    def sext(self, size):
-        return IntValue(int(self), size)
-
-    def split(self, *sizes):
-        assert sum(sizes) == self.size
-        offset = 0
-        for size in sizes:
-            yield IntValue(self.value >> 8 * offset, size)
-            offset += size
-
-    def __int__(self):
-        t = 2 ** (self.size * 8 - 1)
-        return (self.value + t) % (2 * t) - t
-
-    def __str__(self):
-        return f"{int(self)}:i{self.size*8}"
-
-    def __add__(u, v):
-        assert u.size == v.size
-        return IntValue(u.value + v.value, u.size)
-
-    def __sub__(u, v):
-        assert u.size == v.size
-        return IntValue(u.value - v.value, u.size)
-
-
-@dataclasses.dataclass
-class Int:
-    mem: bytearray
-    offset: int
-    size: int
-
-    def split(self, *sizes):
-        assert sum(sizes) == self.size
-        offset = 0
-        for size in sizes:
-            yield Int(self.mem, self.offset + offset, size)
-            offset += size
-
-    @staticmethod
-    def from_value(value):
-        self = Int(bytearray(value.size), 0, value.size)
-        self.value = value
-        return self
-
-    @property
-    def value(self):
-        mem, offset, size = self.mem, self.offset, self.size
-        value = int.from_bytes(mem[offset : offset + size], "little")
-        return IntValue(value, size)
-
-    @value.setter
-    def value(self, value):
-        assert value.size == self.size
-        mem, offset, size = self.mem, self.offset, self.size
-        mem[offset : offset + size] = (value.value).to_bytes(size, "little")
-
-    def __int__(self):
-        return int(self.value)
 
 
 def generate_registers():
