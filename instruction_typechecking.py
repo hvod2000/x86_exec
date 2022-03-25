6913d5853781a382f39cfc243c10cbc2c50ba044
Uladzislau Khamkou <kham@tuta.io>
2022-03-26 01:08:36 +0300

    support immediate values as mov operands

--- instruction_typechecking.py
+++ instruction_typechecking.py
@@ -5,17 +5,6 @@ def typecheck(instruction, vars, regs):
     operation, args = instruction.operation, instruction.args
     if operation == "mov" and len(args) == 2:
         if (
-            args[0] in vars
-            and args[1] in regs
-            and vars[args[0]][1] == regs[args[1]][1]
-        ):
-            size = vars[args[0]][1]
-            args[0] = (args[0], size)
-            args[1] = (args[1], size)
-            return True
-
-    if operation == "mov" and len(args) == 2:
-        if (
             args[0] in regs
             and args[1] in regs
             and regs[args[0]][1] == regs[args[1]][1]
@@ -31,6 +20,29 @@ def typecheck(instruction, vars, regs):
             and regs[args[0]][1] == vars[args[1]][1]
         ):
             size = regs[args[0]][1]
+            args[0] = (args[0], size)
+            args[1] = (args[1], size)
+            return True
+
+        if args[0] in regs and is_int(args[1]):
+            size = regs[args[0]][1]
+            args[0] = (args[0], size)
+            args[1] = (args[1], size)
+            return True
+
+    if operation == "mov" and len(args) == 2:
+        if (
+            args[0] in vars
+            and args[1] in regs
+            and vars[args[0]][1] == regs[args[1]][1]
+        ):
+            size = vars[args[0]][1]
+            args[0] = (args[0], size)
+            args[1] = (args[1], size)
+            return True
+
+        if args[0] in vars and is_int(args[1]):
+            size = vars[args[0]][1]
             args[0] = (args[0], size)
             args[1] = (args[1], size)
             return True
