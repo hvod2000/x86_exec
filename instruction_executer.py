dafd5afb748e1fce515fc8a26655ebccc756eb35
Uladzislau Khamkou <kham@tuta.io>
2022-03-26 11:25:39 +0300

    optimize instruction executer

--- instruction_executer.py
+++ instruction_executer.py
@@ -1,10 +1,13 @@
-dict2fun = lambda dct: lambda name: dct[name]
+from itertools import product
 
 
-def execute(instruction, vars):
-    op, args, get_var = instruction.operation, instruction.args, dict2fun(vars)
-    ax, al, dx = get_var(("ax", 2)), get_var(("al", 1)), get_var(("dx", 2))
-    ax_dx = ax @ dx
+def execute(instruction, namespace):
+    op, args = instruction.operation, instruction.args
+    get_var = lambda var: namespace[var]
+    get_sized = lambda var: (namespace[var], var[1])
+    regs = product("ad", zip("xlh", (2, 1, 1)))
+    ax, al, ah, dx, dl, dh = (namespace[n + m, s] for n, (m, s) in regs)
+    a = [None, al, ax, ax @ dl, ax @ dx]
     match op:
         case "mov":
             target, source = map(get_var, args)
@@ -20,14 +23,8 @@ def execute(instruction, vars):
             target, source = map(get_var, args)
             target @= target.i - source.i
         case "mul":
-            operand = get_var(args[0])
-            if operand.size == 2:
-                ax_dx @= ax.u * operand.u
-            else:
-                ax @= al.u * operand.u
+            operand, size = get_sized(args[0])
+            a[2 * size] @= a[size].u * operand.u
         case "imul":
-            operand = get_var(args[0])
-            if operand.size == 2:
-                ax_dx @= ax.i * operand.i
-            else:
-                ax @= al.i * operand.i
+            operand, size = get_sized(args[0])
+            a[2 * size] @= a[size].i * operand.i
