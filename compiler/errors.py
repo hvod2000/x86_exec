048dfeb36ebdd1958a7eb8eb77bd217e3df931fc
Uladzislau Khamkou <kham@tuta.io>
2022-06-11 12:22:11 +0300

    implement exceptions for compiler

---
+++ compiler/errors.py
@@ -0,0 +1,13 @@
+def warnging(msg):
+    print(f"\033[33mWARNING: {msg}\033[0m")
+
+
+def error(msg):
+    print(f"\033[31m ERRROR: {msg}\033[0m")
+    exit(42)
+
+class DslSyntaxError(Exception):
+    def __init__(self, pos, msg):
+        self.pos = pos
+        self.msg = msg
+        super().__init__(msg)
